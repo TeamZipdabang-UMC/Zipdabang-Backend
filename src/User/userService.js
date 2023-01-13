7faf82eedea037d0a45a24ff19e12ee964f94352
CYY1007 <younguk86@gmail.com>
2023-01-13 17:00:27 +0900

    (update) kakao social login

--- src/User/userService.js
+++ src/User/userService.js
@@ -1,18 +1,34 @@
 import { checkExistEmail } from "./userProvider"
 import pool from "../../config/database"
 import { createUserEmail } from "./userDao";
+import jwt from "jsonwebtoken"
+import privateInfo from "../../config/privateInfo";
 
 
 
 export const startWithKakao = async(userProfile, userEmail)=>{
 
     const isLogin = await checkExistEmail(userEmail)
-    // 1차 회원가입 프로세스
     if (!isLogin){
         const connection = await pool.getConnection(async conn => conn);
         const result = await createUserEmail(connection, userEmail, userProfile);
+        connection.release();
         if (result > 0)
             return "join"
     }
+    else{
+        // 이미 이메일이 있다? -> 로그인 시키기
+        // 토큰에는 id, 이메일만 담을것임!
+        let token = await jwt.sign({
+            userId : isLogin,
+            userEmail,
+        },
+        privateInfo.JWT_SECRET,
+        {
+            expiresIn : "30d",
+            subject : "userInfo"
+        });
 
+        return token
+    }
 }
