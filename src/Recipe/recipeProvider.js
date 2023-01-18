99828d1e5b741c3b53f2fd18bd223a781a94b27f
minseong0609 <som4uuu1@gmail.com>
2023-01-18 21:50:20 +0900

    (add) connection release

--- src/Recipe/recipeProvider.js
+++ src/Recipe/recipeProvider.js
@@ -4,23 +4,27 @@ import { getCategoryList, getThumbCategoryList, getCategoryPagingList, getMainCa
 export const getCategoryID = async(categoryId, is_official)=>{
     const connection = await pool.getConnection(async conn => conn);
     const result = await getCategoryList(connection, categoryId, is_official);
+    connection.release();
     return result
 }
 
 export const getMainCategoryID = async(categoryId)=>{
     const connection = await pool.getConnection(async conn => conn);
     const result = await getMainCategoryList(connection, categoryId);
+    connection.release();
     return result
 }
 
 export const getThumbCategoryID = async(categoryId)=>{
     const connection = await pool.getConnection(async conn => conn);
     const result = await getThumbCategoryList(connection, categoryId);
+    connection.release();
     return result
 }
 
 export const getCategoryPagingID = async(categoryId, last)=>{
     const connection = await pool.getConnection(async conn => conn);
     const result = await getCategoryPagingList(connection, categoryId, last);
+    connection.release();
     return result
 }
