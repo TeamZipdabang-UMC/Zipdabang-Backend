a1df76d0af8bbeae8cefcd2e782128ce21589312
minseong0609 <som4uuu1@gmail.com>
2023-01-19 12:44:33 +0900

    (add) create root

--- config/express.js
+++ config/express.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import userRouter from "../src/User/userRouter";
 import recipeRouter from "../src/Recipe/recipeRouter";
+import rootRouter from "../src/Root/rootRouter";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 app.use('/users', userRouter);
-
+app.use('/root', rootRouter);
+app.use('/recipes', recipeRouter);
 
 export default app;
